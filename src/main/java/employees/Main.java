5ca2e5397a06fdd49abf4d5c645c8820c49e10eb
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-10 21:59:02 -0500

    Employees program

--- src/main/java/employees/Main.java
+++ src/main/java/employees/Main.java
@@ -1,5 +1,6 @@
 package employees;
 
+import java.text.NumberFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,20 +8,27 @@ public class Main {
     public static void main(String[] args) {
 
         String people = """
-                Flinstone, Fred, 1/01/1900
-                Rubble, Barney, 2/02/1905
-                Flinstone, Wilma, 3/03/1910
-                Rubble, Betty, 4/4/1915
+                Flinstone, Fred, 1/01/1900, Programmer
+                Rubble, Barney, 2/02/1905, Manager
+                Flinstone, Wilma, 3/03/1910, Analyst
+                Rubble, Betty, 4/4/1915, CEO
                 """;
 
-        String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4})\\n";
+        String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)\\n";
         Pattern pat = Pattern.compile(regex);
         Matcher mat = pat.matcher(people);
 
-
+        int totalSalaries = 0;
         while (mat.find()) {
-            System.out.printf("%s %s %s%n", mat.group("firstName"), mat.group("lastName"), mat.group("dob"));
-
+           totalSalaries+= switch (mat.group("role")) {
+                case "Programmer" -> 3000;
+                case "Manager" -> 3500;
+                case "Analyst" -> 2500;
+                case "CEO" -> 5000;
+               default -> 0;
+            };
         }
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+        System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
     }
 }
