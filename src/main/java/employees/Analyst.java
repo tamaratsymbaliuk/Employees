2a0673e146e85801f52603028378d9549d8b1d1f
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-14 13:19:24 -0500

    Employees program - Analyst Class

--- src/main/java/employees/Analyst.java
+++ src/main/java/employees/Analyst.java
@@ -1,42 +1,22 @@
 package employees;
 
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Analyst {
-    private String lastName;
-    private String firstName;
-    private LocalDate dob;
+public class Analyst extends Employee {
     private int projectCount = 0;
 
     private final String analystRegex = "\\w+=(?<orgSize>\\w+),\\w+=(?<dr>\\w+)";
     private final Pattern analystPat = Pattern.compile(analystRegex);
-    private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
-    private final Pattern peoplePat = Pattern.compile(peopleRegex);
-    private final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-    private final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
 
     public Analyst(String personText) {
-        Matcher peopleMat = peoplePat.matcher(personText);
-        if (peopleMat.find()) {
-            this.lastName = peopleMat.group("lastName");
-            this.firstName = peopleMat.group("firstName");
-            this.dob = LocalDate.from(dtFormatter.parse(peopleMat.group("dob")));
+        super(personText);
             Matcher analystMat = analystPat.matcher(peopleMat.group("details"));
             if (analystMat.find()) {
                 this.projectCount = Integer.parseInt(analystMat.group("projectCount"));
             }
         }
-    }
     public int getSalary() {
         return 2500 + projectCount * 2;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
     }
 }
