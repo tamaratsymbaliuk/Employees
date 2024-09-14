522999bed5c9815cc266f076ec48e790467268e4
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-14 12:20:24 -0500

    Employees program - Manager Class

--- src/main/java/employees/Manager.java
+++ src/main/java/employees/Manager.java
@@ -1,44 +1,25 @@
 package employees;
 
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Manager {
-    private String lastName;
-    private String firstName;
-    private LocalDate dob;
+public class Manager extends Employee {
     private int orgSize = 0;
     private int directReports = 0;
 
     private final String mgrRegex = "\\w+=(?<orgSize>\\w+),\\w+=(?<dr>\\w+)";
     private final Pattern mgrPat = Pattern.compile(mgrRegex);
-    private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
-    private final Pattern peoplePat = Pattern.compile(peopleRegex);
-    private final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-    private final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
 
     public Manager(String personText) {
-        Matcher peopleMat = peoplePat.matcher(personText);
-        if (peopleMat.find()) {
-            this.lastName = peopleMat.group("lastName");
-            this.firstName = peopleMat.group("firstName");
-            this.dob = LocalDate.from(dtFormatter.parse(peopleMat.group("dob")));
+        super(personText); // super calls the constructor of the parent class
             Matcher mgrMat = mgrPat.matcher(peopleMat.group("details"));
             if (mgrMat.find()) {
                 this.orgSize = Integer.parseInt(mgrMat.group("orgSize"));
                 this.directReports = Integer.parseInt(mgrMat.group("dr"));
             }
         }
-    }
+
     public int getSalary() {
         return 3500 + orgSize * directReports;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
     }
 }
