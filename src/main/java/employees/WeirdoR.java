fc40b2456f3b376d433268f014f598d04e659168
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-17 22:28:19 -0500

    Employees program - Programmer Class

--- src/main/java/employees/WeirdoR.java
+++ src/main/java/employees/WeirdoR.java
@@ -2,7 +2,12 @@ package employees;
 
 import java.time.LocalDate;
 
-public record WeirdoR(String lastName, String firstName, LocalDate dob) {
+public record WeirdoR(String lastName, String firstName, LocalDate dob)  {
     // look at the Weirdo class, the record provides constructor, getter,(no setters) etc automatically
-    // record and fields within it are final and can't be extended
+    // record and fields within it are final and record can't be extended to other classes hence not abstract
+    // custom constructor
+    public WeirdoR(String lastName, String firstName) {
+        this(lastName, firstName, LocalDate.of(1,1,1));
+    }
+
 }
