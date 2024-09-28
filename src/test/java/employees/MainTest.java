548e8e1625689284362e8dd64ca9c981bff918f0
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-27 21:26:53 -0500

    Employees program - MainTest

---
+++ src/test/java/employees/MainTest.java
@@ -0,0 +1,18 @@
+package employees;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void testNameToSalary() {
+        Main main = new Main();
+        main.main(new String[0]);
+        int salary = main.getSalary("Wilma");
+        assertEquals(2500, salary);
+    }
+
+
+}
