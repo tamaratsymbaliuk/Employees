7acf349721a699b9ec56e230604a159d71650431
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-29 16:08:02 -0500

    Employees program - MainTest

--- src/test/java/employees/MainTest.java
+++ src/test/java/employees/MainTest.java
@@ -14,5 +14,15 @@ class MainTest {
         assertEquals(2500, salary);
     }
 
+    @Test
+    public void testInvalidNameToSalary() {
+        Main main = new Main();
+        main.main(new String[0]);
+        int salary = main.getSalary("Uma");
+        assertEquals(-1, salary);
+    }
+
+
+
 
 }
