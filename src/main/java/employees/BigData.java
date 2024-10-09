0a8c3c0b5278890af03c0af3f29f0c522d2a169a
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-08 21:50:37 -0500

    Employees program - BigData

--- src/main/java/employees/BigData.java
+++ src/main/java/employees/BigData.java
@@ -3,22 +3,31 @@ package employees;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 public class BigData {
+
+    record Person(String firstName, String lastName, long salary, String state) {}
     public static void main(String[] args) {
+
         try {
             long startTime = System.currentTimeMillis();
-            Long result = Files.lines(Path.of("/Users/tamaratsymbaliuk/Downloads/Hr5m.csv")).parallel()
+           Map<String, String> result = Files.lines(Path.of("/Users/tamaratsymbaliuk/Downloads/Hr5m.csv")).parallel()
                     .skip(1)// header row
                     .limit(10)
                     .map(s -> s.split(",")) //splitting stream of strings into arrays
-                    .map(arr -> arr[25]) // getting column 25 from the array
-                    .mapToLong(sal -> Long.parseLong(sal))
-                    .sum();
-//                    .collect(Collectors.summingLong(sal -> Long.parseLong(sal)));
+                    .map(a -> new Person(a[2], a[4], Long.parseLong(a[25]),a[32]))
+       //             .collect(summingLong(Person::salary));
+                    .collect(groupingBy(Person::state, TreeMap::new,
+                            collectingAndThen(summingLong(Person::salary), s -> String.format("$%,d.00%n", s)))); // using TreeMap is not necessary, using to have result alphabetize
             long endTime = System.currentTimeMillis();
-            System.out.printf("$%,d.00%n", result);
+      //      System.out.printf("$%,d.00%n", result);
+            System.out.println(result);
             System.out.println(endTime - startTime);
                  //   .forEach(System.out::println);
         } catch (IOException e) {
